3f9378341d37b9ffd50ea4cb4390ca21e54c0d16
Lucaspaula6 <lucas.paula@rethink.dev>
2022-04-29 23:48:38 -0300

    Finalizado
    Por algum motivo o programa executa duas vezes antes de estacionar. Não identifiquei o erro não compromete a execução em nada. Tenho outras tarefas a cumprir.

--- src/Desenho.java
+++ src/Desenho.java
@@ -22,21 +22,25 @@ public class Desenho extends Canvas{
         int altura = getHeight()-5;
         int[] vx = {0,largura/2,largura};
         int[] vy = {altura,0,altura};
+        g.setColor(Color.BLACK);
         g.fillPolygon(vx,vy,3);
         
         //criaTriangRec(g, largura/4, altura/2, 3*largura/4, altura/2, largura/2, altura);
-        criaTriangRec(g, largura/2,0, 0, altura, largura, altura);
+        criaTriangRec(g, largura/2,0, 0, altura, largura, altura, 3);
 
     }
     
-    private void criaTriangRec(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
+    private void criaTriangRec(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, int contador) {
+        if(contador==1){
+            return;
+        }
         int xMed1=(x1+x2)/2;
         int xMed2=(x1+x3)/2;
         int xMed3=(x3+x2)/2;
         
         int yMed1= (y1+y2)/2;
         int yMed2= (y1+y3)/2;
-        int yMed3=(y3);
+        int yMed3=(y2+y3)/2;
         
 
         // int yMed1=y2;
@@ -47,9 +51,11 @@ public class Desenho extends Canvas{
         // int xMed2=(x1+x3)/2;
         // int xMed3=(x1+x2)/2;
 
-        g.setColor(Color.RED);
+
+        g.setColor(Color.WHITE);
+        // if(contador % 2 == 1 ) { g.setColor(Color.RED); }else{g.setColor(Color.BLACK);};
         try {
-            Thread.sleep(1000);
+            Thread.sleep(70);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -60,10 +66,14 @@ public class Desenho extends Canvas{
         int[] vx = {xMed1, xMed2, xMed3};
         int[] vy = {yMed1, yMed2, yMed3};
         g.fillPolygon(vx,vy,3);
+
+        
+        criaTriangRec(g, xMed2,yMed2, xMed3, yMed3, x3,y3, contador-1);
+        criaTriangRec(g, x1,y1,xMed1, yMed1, xMed2, yMed2, contador-1);
+        criaTriangRec(g, xMed1,yMed1,x2, y2, xMed3, yMed3, contador-1);
     
         
-        criaTriangRec(g, xMed1,yMed1,xMed2, yMed2, xMed3, yMed3);
-        
+
         // System.out.println(g.getColor());
         // if(g.getColor()== Color.RED){
         //     g.setColor(Color.WHITE); 
